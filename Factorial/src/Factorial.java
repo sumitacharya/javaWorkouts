b677cae44a81c4bb5f4c99f1f60635577b46119a
sumit <sumit.soome@gmail.com>
2015-10-05 17:55:16 +0200

    Comments added in few programs

--- Factorial/src/Factorial.java
+++ Factorial/src/Factorial.java
@@ -5,13 +5,14 @@ public class Factorial {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter a number : ");
 		int num = input.nextInt();
 		System.out.println("The factorial of " + num +" is : " + factorial(num));
 	}
-	
+	/*
+	 * Calculates the factorial using recursive approach.
+	 */
 	public static int factorial(int n){
 		if (n == 0) return 1; 
 		else return n * factorial(n-1);
